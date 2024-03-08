a058b7b08636eba5eecfacf793e13defe47bdf85
self esteem <selfesteem@gmail.com>
2024-03-08 10:17:29 +0900

    finished my code see what he do

--- src/ReactQuiz.js
+++ src/ReactQuiz.js
@@ -1,28 +1,34 @@
+// noinspection JSCheckFunctionSignatures
+
 import {useEffect, useReducer, useState} from "react";
 import quizJSON from "./questions.json";
+import Header from "./Header";
 
 const quiz = quizJSON.questions;
-const initialState = {count: 0, step: 1, answer: undefined}
+const initialState =
+  {
+    points: 0, step: 1, answer: undefined,
+    isEnd: false, countSec: 30, highScore: 5, isAnswered: false
+  }
 
 function reducer(state, action) {
-  console.log('state => ', state, 'action => ', action, "lll");
-
   switch (action.type) {
-    case 'dec':
-      return {...state, count: state.count - action.payload};
-    case 'inc' :
-      return {...state, count: state.count + action.payload};
-    case 'setCount' :
-      return {...state, count: state.count + action.payload};
+    case 'setPoint' :
+      return {...state, points: state.points + action.payload};
     case 'setStep' :
-      return {...state, step: state.step + action.payload};
+      return {...state, step: state.step + 1};
     case 'setAnswer' :
       return {...state, answer: action.payload};
-    case 'next' :
-      return {
-        ...state,
-        step: state.step + action.payload[0], answer: action.payload[1]
-      };
+    case 'setIsAnswered' :
+      return {...state, isAnswered: action.payload};
+    case 'clearAnswer' :
+      return {...state, answer: undefined};
+    case 'setIsEnd' :
+      return {...state, isEnd: true};
+    case 'setCountSec' :
+      return {...state, countSec: state.countSec - 1};
+    case 'setHighScore' :
+      return {...state, highScore: action.payload};
     case 'reset' :
       return initialState;
     default:
@@ -33,118 +39,132 @@ function reducer(state, action) {
 function ReactQuiz() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  const {step, count, answer} = state;
+  const {step, points, answer, isEnd, countSec, highScore, isAnswered}
+    = state;
+
   const answerIndex = quiz[step - 1].correctOption
   const correctAnswer = quiz[step - 1].options[answerIndex];
 
-  const date = new Date();
-  date.setDate(date.getDate() + count);
+  useEffect(() => {
+    const intervalId = setInterval(function () {
+      dispatch({type: 'setCountSec'})
+    }, 1000);
+    if (countSec === 0) {
+      clearInterval(intervalId);
+      dispatch({type: "setIsEnd"});
+    }
+    return () => clearInterval(intervalId);
+  }, [countSec]);
 
-  const dec = function () {
-    dispatch({type: 'dec', payload: step});
-  };
+  useEffect(() => {
+    const highScore = localStorage.getItem('highScore');
+    if (highScore)
+      dispatch({type: 'setHighScore', payload: +highScore})
+  }, []);
+
   const next = function () {
-    if(step === 15) return;
-    dispatch({type: 'next', payload: [1, undefined]});
+    if (step === 15)
+      return dispatch({type: 'setIsEnd'});
+    dispatch({type: 'setIsAnswered', payload: false});
+    dispatch({type: 'setStep'})
   }
 
   const defineStep = (e) =>
     dispatch({type: 'setStep', payload: +e.target.value});
 
-  const reset = function () {
-    dispatch({type: 'reset'})
-  };
+  function handleSelect(selection) {
+    dispatch({type: 'setAnswer', payload: selection});
+    dispatch({type: 'setIsAnswered', payload: true});
 
-  function handleAnswer(option) {
-    console.log("선택한 답변 : ", option)
-    dispatch({type: 'setAnswer', payload: option})
-    if (correctAnswer === option) {
-      dispatch({type: 'setCount', payload: quiz.at(step).points})
+    if (!isAnswered && correctAnswer === selection)
+      dispatch({type: 'setPoint', payload: quiz.at(step - 1).points});
+
+    if (isAnswered) dispatch({type: 'setPoint', payload: 0});
+
+    if (points > highScore) {
+      localStorage.setItem('highScore', points)
+      dispatch({type: 'setHighScore', payload: points})
     }
-    setTimeout(function () {
-      next();
-    },3000);
+  }
+
+  function handleReload() {
+    dispatch({type: 'reset'})
+    window.location.reload();
   }
 
   return (
     <>
-      <Head/>
-      <div className='main'>
-        <Progress step={step} count={count} defineStep={defineStep}/>
-        <Questions step={step} onAnswer={handleAnswer}
-                   answer={answer} correctAnswer={correctAnswer}/>
-        <NextBtn dec={dec} next={next}/>
-      </div>
+      <Header/>
+      {!isEnd &&
+        <div className='main'>
+          <Progress step={step} point={points} defineStep={defineStep}/>
+          <Questions step={step} onSelect={handleSelect}
+                     answer={answer} correctAnswer={correctAnswer}
+                     isAnswered={isAnswered}
+          />
+          <NextBtn countSec={countSec} next={next}/>
+        </div>}
+      {isEnd && <Result points={points} highScore={highScore} onReload={handleReload}/>}
     </>
   )
-    ;
 }
 
 export default ReactQuiz;
 
-function Progress({step, count, defineStep}) {
-  console.log('step ==> ', step)
+function Progress({step, point, defineStep}) {
+
   return (
     <div className='progress'>
-      <input className='bar' type="range" min="1" max={`${quiz.length}`} value={+step}
+      <input className='bar' type="range" min="1"
+             max={`${quiz.length}`} value={+step}
              onChange={defineStep}/>
       <div className='range'>
         <span>{step}/{quiz.length}</span>
-        <span>{count}/{quiz.reduce((acc, curr) => curr.points + acc, 0)} point</span>
+        <span>{point}/{quiz.reduce((acc, curr) => curr.points + acc, 0)} point</span>
       </div>
     </div>
   )
 }
 
-function Questions({step, onAnswer, answer, correctAnswer}) {
+function Questions({step, onSelect, correctAnswer, isAnswered}) {
   const qa = quiz[step - 1];
-  console.log("정답은 요 => ", correctAnswer)
-  console.log("답변은 요 => ", answer)
+
   return (
     <ul className='options'>
       <h4>{qa.question}</h4>
-      {qa.options.map(option =>
-        <li key={option.question}
-            className={`btn ${answer && (option === correctAnswer ? 'btn-option correct'
-              : 'btn-option wrong')}`}
-            onClick={() => onAnswer(option)}>
-          {option}
-        </li>)}
+      <ul>
+        {qa.options.map(option =>
+          <li key={option.question}
+              className={`btn answer ${isAnswered &&
+              (option === correctAnswer ? 'btn-option correct'
+                : 'btn-option wrong')}`}
+              disabled={isAnswered}
+              onClick={() => onSelect(option)}>
+            {option}
+          </li>)}
+      </ul>
     </ul>)
 }
 
-
-function NextBtn({next}) {
+function NextBtn({next, countSec}) {
   return (
     <div className='btn-ui'>
-      <button className='timer'><TimeoutBtn/></button>
+      <button className='timer'>
+        {Math.floor(countSec / 60)} : {countSec % 60}</button>
       <button className='btn' onClick={next}>Next</button>
     </div>
   )
 }
 
-function TimeoutBtn() {
-  const [sec, setSec] = useState(7)
-  useEffect(() => {
-    const intervalId = setInterval(function () {
-      setSec(sec => sec - 1)
-    }, 1000);
-    if (sec === 0) {
-      clearInterval(intervalId)
-    }
-    return () => clearInterval(intervalId);
-  }, [sec]);
-
-  return <div>
-    {Math.floor(sec / 60)} : {sec % 60}
-  </div>
-}
-
-function Head() {
+function Result({points, highScore, onReload}) {
   return (
-    <div className={'app-header'}>
-      <img src='/logo192.png' alt='logo img'/>
-      <h1>The react quiz</h1>
+    <div className='main'>
+      <div className='loader-container result'>
+        <div className='high-score'>최고 점수는 {highScore}점 입니다.</div>
+        <span>당신은 총 {points}점을 얻었습니다.</span>
+        <button onClick={onReload} className='btn'>Reset</button>
+        {points > highScore && <div>최고 점수를 경신 했습니다.</div>}
+      </div>
     </div>
   )
 }
